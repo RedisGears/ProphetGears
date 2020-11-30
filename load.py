3836e70f4f3add5eba7a45699c4461e759705880
Guy Korland <gkorland@gmail.com>
2020-11-30 18:56:59 +0200

    fix delete

--- load.py
+++ load.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 key = 'test'
 rts = Client()
-rts.delete(key)
+rts.redis.delete(key)
 rts.create(key, labels={'source': key})
 df = pd.read_csv('samples.csv')
 for _, row in df.iterrows():
